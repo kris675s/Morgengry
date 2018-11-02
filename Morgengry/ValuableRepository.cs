a64a556bd0d8c14a092eab01cc4ebbc1ffe0eb13
kris675s <kris675s@eul.eal.dk>
2018-11-02 09:14:18 +0100

    IValuable + repo

---
+++ Morgengry/ValuableRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class ValuableRepository : IValuable
+    {
+        private List<IValuable> valueables = new List<IValuable>();
+
+        public AddValuable(valuables);
+        public string GetValuable : IValuable;
+        public double GetTotalValue;
+        public int Count;
+        
+
+    }
+}
